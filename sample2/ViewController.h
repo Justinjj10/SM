30b4ec00b86f0d26dc50e6a9125465ee32937629
Mac <mac@macs-macbook-3.local>
2016-12-08 19:30:00 +0800

    Initial Commit

---
+++ sample2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample2
+//
+//  Created by Mac on 8/12/16.
+//  Copyright © 2016 Machk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
